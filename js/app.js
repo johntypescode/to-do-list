745a1c6223dcec797e0046237f5b80510a4905c0
John <186672498+johntypescode@users.noreply.github.com>
2025-02-17 04:19:22 -0500

    Implementation and initial testing of ()to generate HTML elements in the DOM on the fly.

--- js/app.js
+++ js/app.js
@@ -19,5 +19,28 @@ window.onload = () => {
     const new_task_input = $('input#new-task');
     const add_task_bttn = $('input#add-task');
 
+    /**
+     * Generate an HTML element in the DOM.
+     * ====================================
+     */
+    const $html = (tag_type, parent_of, attrs = {}) => {
+        const new_html_element = document.createElement(tag_type);
+
+        if(Object.keys(attrs).length > 0) {
+            const attrs_to_set = Object.entries(attrs);
+            for(const [key, value] of attrs_to_set) {
+                const check_key = key.toLowerCase();
+                if(check_key === 'innerhtml') new_html_element.innerHTML = attrs[key];
+                else if(check_key === 'innertext') new_html_element.innerText = attrs[key];
+                else if(check_key === 'textcontent') new_html_element.textContent = attrs[key];
+                else new_html_element.setAttribute(key, value);
+            }
+        }
+
+        return parent_of.appendChild(new_html_element);
+    };
+
+    // Initial test for `$html()`
+    $html('p', $('body'), { innerText: 'You only see me because of JavaScript! :0'});
 
 };
